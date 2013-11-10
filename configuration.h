1febf3ffba765a0002e7e29ec5062fe41aabe007
= <=>
2013-11-10 14:49:08 -0900

    Basic window made.

---
+++ configuration.h
@@ -0,0 +1,16 @@
+#ifndef TSCONF_CONFIGURATION_HEAD
+#define TSCONF_CONFIGURATION_HEAD
+
+namespace tsconf {
+
+    static const int32_t windowWidth = 440;
+    static const int32_t windowHeight = 512;
+
+    static const int32_t smallWidgetHeight = 30;
+    static const int32_t largeWidgetHeight = 52;
+
+    static const char * windowTitle = "TurboSphere Configuration";
+
+}
+
+#endif
--- tsconf-main.cpp
+++ tsconf-main.cpp
@@ -1,7 +1,38 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Menu_Bar.H>
+#include <FL/Fl_Group.H>
 
+#include "configuration.h"
 
+void Quit(Fl_Widget *unused0, void *unused1){
+    exit(0);
+}
+
+void DoNothing(Fl_Widget *unused0, void *unused1){
+
+}
+
+void createMenuBar(){
+    static Fl_Menu_Item menuItems[] = {
+        {"&File", 0, 0, 0, FL_SUBMENU},
+        {"&Save\t", FL_COMMAND + 's', (Fl_Callback *)DoNothing, 0},
+        {"&Revert\t", FL_COMMAND + 'z', (Fl_Callback *)DoNothing, 0},
+        {"E&xit\t", FL_COMMAND + 'q', (Fl_Callback *)Quit, 0},
+        {0}
+    };
+    Fl_Menu_Bar *mainMenu = new Fl_Menu_Bar(0, 0, tsconf::windowWidth, tsconf::smallWidgetHeight);
+    mainMenu->copy(menuItems);
+}
 
 
 int main(int argc, char *argv[]){
-    return 0;
+    Fl_Window confWindow(tsconf::windowWidth, tsconf::windowHeight, tsconf::windowTitle);
+    confWindow.begin();
+        createMenuBar();
+    confWindow.end();
+
+    confWindow.show();
+
+    return Fl::run();
 }
